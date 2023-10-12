ade8c998e4d93b4ad921ffd20fc8d7a052be31ab
karthickangusamy7@gmail.com <Ayyappan>
2023-10-12 22:56:41 +0530

    Ecommerce website

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     const navigate = useNavigate()
   return (
     <div className='p-3 bg-secondary'>
-        <h3>Ecommerce Website</h3>
+        <h3>Shopping Paradise</h3>
         <div className='row justify-content-center pt-2 pb-1'>
             <div className='col-sm-12 col-md-7 col-lg-6 col-xl-5 d-flex '>
                 <button className='btn btn-success me-4' onClick={() => navigate('/')}>Home</button>
